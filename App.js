ce9e27de0dd8f8c24d4e0a31acc76057e9eb9b55
Gunvolt666 <40903940+Gunvolt666@users.noreply.github.com>
2018-07-07 06:03:26 -0500

    Add files via upload
    Lamento que se repita es que han surgido errores asi que actualizamos el app.js a un nuevo proyecto

--- App.js
+++ App.js
@@ -3,30 +3,61 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  var successBool = window.navigator.vibrate(patron);
-    navigator.vibrate(1000);
+  Programmadvance()
+  {
+   
+(function () {
+    "uso estricto";
+    var accelerometer;
+    var WinJS;
+    var Windows;
+    var app = WinJS.Application;
 
-  navigator.vibrate([1000,500,2000]);
+    // Esta funcion responde a todas las activaciones de la app
+    app.onactivated = function (eventObject) {
+        if (eventObject.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.launch) {
+            if (accelerometer == null) {
+                accelerometer = Windows.Devices.Sensors.Accelerometer.getDefault();
+
+                // Establece el reporte del intervalo
+                var minimumReportInterval = accelerometer.minimumReportInterval;
+                var reportInterval = minimumReportInterval > 16 ? minimumReportInterval : 16;
+                accelerometer.reportInterval = reportInterval;
+
+                accelerometer.addEventListener("readingchanged", onDataChanged);
+            }
+            WinJS.UI.processAll();
+        }
+    };
 
 
-  //DETENER VIBRACION
-  navigator.vibrate(0);
-  navigator.vibrate([]);
+    function onDataChanged(e) {
+        var reading = e.reading;
+        var accelX = reading.accelerationX;
+        var accelY = reading.accelerationY;
+        var accelZ = reading.accelerationZ;
 
-  navigator.vibrate([0]);
+        id('eventOutputX').innerHTML = accelX.toFixed(2);
+        id('eventOutputY').innerHTML = accelY.toFixed(2);
+        id('eventOutputZ').innerHTML = accelZ.toFixed(2);
+    }
+
+   
+    function id(elementId) {
+        return document.getElementById(elementId);
+    }
+
+    app.start();
+})();
 
 
-  //DETECTANDO API
-
-  var soporte = "vibrate" in navigator;
-
-  
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Health4U</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
